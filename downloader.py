3c82e00f017a631eba93a5195b3fe992a5a4078a
JakubLaza <jakub.laza23@gmail.com>
2021-12-27 02:21:08 +0100

    add get_total_pages

--- downloader.py
+++ downloader.py
@@ -2,44 +2,51 @@ import requests
 import time
 import pandas as pd
 
+token = "8a0ff681501b0bac557bf90fe6a036f7"
+
 def counter(func):
   def wrapper(*args, **kwargs):
     wrapper.count += 1
     if wrapper.count == 998:
         print("Counter on 998!")
+        time.sleep(2500)
+        return func(*args, **kwargs)
     else:     
-    # Call the function being decorated and return the result
         return func(*args, **kwargs)
   wrapper.count = 1
-  # Return the new decorated function
   return wrapper
 
-token = "8a0ff681501b0bac557bf90fe6a036f7"
+
 @counter
 def request(url, token):
             r = requests.get(url, {"apiToken": token})
             return r 
 
+def get_total_pages(token):
+    url = "https://onemocneni-aktualne.mzcr.cz/api/v3/osoby?page=1&itemsPerPage=1000&datum%5Bbefore%5D=24.12.2021&datum%5Bafter%5D=1.1.2020"
+    r = request(url, token)
+    total_pages = int(r.json()["hydra:view"]["hydra:last"].split("=")[-1])
+    return total_pages
 
 def downloader(token, start_page = 1):
     if start_page == 1:
         df = pd.DataFrame()
     else:
         df = pd.read_parquet('data.gzip', engine='fastparquet')
-    url_0 = "https://onemocneni-aktualne.mzcr.cz/api/v3/osoby?page=1&itemsPerPage=1000&datum%5Bbefore%5D=24.12.2021&datum%5Bafter%5D=1.1.2020"
-    req = request(url_0, token)
-    pages_total = int(req.json()["hydra:view"]["hydra:last"].split("=")[-1])
+    pages_total = get_total_pages(token)
     for i in range(start_page, pages_total+1):
         url = "https://onemocneni-aktualne.mzcr.cz/api/v3/osoby?page="+ str(i) +"&itemsPerPage=1000&datum%5Bbefore%5D=24.12.2021&datum%5Bafter%5D=1.1.2020"
         r = request(url, token)
         if r.status_code == 200:
             df = df.append(pd.DataFrame.from_dict(r.json()["hydra:member"]))
-        if r.status_code == 429:
+        elif r.status_code == 429:
             msg = r.json()["message"]
             t = "".join(a for a in msg if a.isdigit())
             print("Now waiting for " + str(t+60)+ " seconds...")
             time.sleep(t+60)
             request.count = 1
+            r = request(url, token)
+            df = df.append(pd.DataFrame.from_dict(r.json()["hydra:member"]))
         else:
             print(r.status_code)
             print(r.text)
@@ -51,4 +58,6 @@ def downloader(token, start_page = 1):
              df.to_parquet('data.gzip'.format(a = i),compression='gzip')
     print("DONE")
     return df
-data = downloader(token)
+
+df.to_parquet('data.gzip'.format(a = i),compression='gzip')
+
