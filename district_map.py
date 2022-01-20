13185bc0721921e344aaa33e1e930c0d8875dd7b
JakubLaza <jakub.laza23@gmail.com>
2022-01-21 00:47:56 +0100

    add web sraper of czsu for districts popualtions

--- district_map.py
+++ district_map.py
@@ -1,7 +1,12 @@
 #### Obtaining data for map of districs of the Czech Republic, given that Prague is officially not a district, but a region. We download data for Prague from region database and add them to districts
 #### Hence we treat Prague as a district. 
-# %%
+#
 import geopandas
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import unicodedata
+
 link_dist = "/Users/Jakub/Downloads/BND/PolbndDistDA.shp"
 districts = geopandas.read_file(link_dist)
 
@@ -18,7 +23,10 @@ districts = districts.rename(columns = {"SHN2":"district_code"})
 
 districts = districts.append(prague).drop(columns = ["NAMA", "DESN", "ISN", "F_CODE", "FCSUBTYPE"]).set_index("NAMN")
 
-for i in range(1, 78):
+for i in range(78):
     districts["district_code"][i] = districts["district_code"][i][:6]
 
-districts.plot()
+
+data = districts.merge(df, left_on = "NAMN", right_index = True, how = "left")
+print(data)
+print(districts)
